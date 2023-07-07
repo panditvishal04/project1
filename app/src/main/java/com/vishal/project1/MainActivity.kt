6c0af5991b08d2a183928990663ebd2e65aafe1d
vishal pandit <121655488+panditvishal04@users.noreply.github.com>
2023-07-07 19:16:06 +0530

    Update MainActivity.kt

--- app/src/main/java/com/vishal/project1/MainActivity.kt
+++ app/src/main/java/com/vishal/project1/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var one :TextView
     private lateinit var travel :TextView
     private lateinit var arrow :ImageView
-    private lateinit var gd : Guideline
     private lateinit var swt_on :Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,32 +30,5 @@ class MainActivity : AppCompatActivity() {
         travel=findViewById(R.id.tv_travel)
         arrow=findViewById(R.id.imgv_a)
         swt_on=findViewById(R.id.swt_on)
-        gd=findViewById(R.id.guideline_horizontal_13)
-        gd=findViewById(R.id.guideline_horizontal_18)
-        gd=findViewById(R.id.guideline_horizontal_20)
-        gd=findViewById(R.id.guideline_horizontal_30)
-        gd=findViewById(R.id.guideline_horizontal_375)
-        gd=findViewById(R.id.guideline_horizontal_40)
-        gd=findViewById(R.id.guideline_horizontal_50)
-        gd=findViewById(R.id.guideline_horizontal_525)
-        gd=findViewById(R.id.guideline_horizontal_48)
-        gd=findViewById(R.id.guideline_horizontal_237)
-        gd=findViewById(R.id.guideline_horizontal_34)
-        gd=findViewById(R.id.guideline_horizontal_43)
-        gd=findViewById(R.id.guideline_horizontal_45)
-       gd=findViewById(R.id.guideline_vertical_10)
-        gd=findViewById(R.id.guideline_vertical_05)
-        gd=findViewById(R.id.guideline_vertical_20)
-        gd=findViewById(R.id.guideline_vertical_30)
-        gd=findViewById(R.id.guideline_vertical_35)
-        gd=findViewById(R.id.guideline_vertical_40)
-        gd=findViewById(R.id.guideline_vertical_45)
-        gd=findViewById(R.id.guideline_vertical_55)
-        gd=findViewById(R.id.guideline_vertical_60)
-        gd=findViewById(R.id.guideline_vertical_50)
-        gd=findViewById(R.id.guideline_vertical_70)
-        gd=findViewById(R.id.guideline_vertical_80)
-        gd=findViewById(R.id.guideline_vertical_90)
-
     }
 }
